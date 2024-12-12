8ed72005aee38e39ae039e13b0b076fb34b285da
Arush Nandakumar <131232484+arushn47@users.noreply.github.com>
2024-12-13 03:42:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 UPLOAD_FOLDER = 'static/images/uploads/'
 RESULTS_FOLDER = 'static/images/results/exp'
+YOLO_SCRIPT = 'yolo/detect.py'
+ARCHIVE_FOLDER = 'static/images/archive'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
@@ -15,6 +17,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(RESULTS_FOLDER, exist_ok=True)
+os.makedirs(ARCHIVE_FOLDER, exist_ok=True)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -30,11 +33,11 @@ def home():
 
             try:
                 subprocess.run(
-                    ['python', 'D:\CODING\Project Exhibition\PE1\yolo\detect.py'], check=True
+                    ['python', YOLO_SCRIPT], check=True
                 )
 
-                archive_folder = os.path.join(os.getcwd(), 'static', 'images', 'archive')
-                shutil.move(file_path, os.path.join(archive_folder, filename))
+                archive_file_path = os.path.join(ARCHIVE_FOLDER, filename)
+                shutil.move(file_path, archive_file_path)
 
                 result_image_path = os.path.join(RESULTS_FOLDER, 'exp', filename)
                 image_path = f"images/results/exp/{filename}"
@@ -55,10 +58,9 @@ def result():
 def cleanup():
     try:
         results_folder = app.config['RESULTS_FOLDER']
-        exp_folder = os.path.join(results_folder)
 
-        if os.path.exists(exp_folder):
-            shutil.rmtree(exp_folder)
+        if os.path.exists(results_folder):
+            shutil.rmtree(results_folder)
 
         return "Cleanup complete", 200
     except Exception as e:
